ac424756e0319cfbdb817baede2c3b877062ff14
Pankaj Bagdare <bagdarepankaj@gmail.com>
2022-06-21 14:29:50 +0530

    remote true jquery-ujs issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
 // import "controllers"
 // import "jquery"
 // import "jquery-ujs"
 // import "@popperjs/core"
 // import "bootstrap"
-// import './jquery'
-// import {} from 'jquery-ujs'
-// import "./controllers"
+import './jquery'
+import 'jquery-ujs'
+import "@hotwired/turbo-rails"
+import "@hotwired/stimulus"
 import * as bootstrap from "bootstrap"
+// import "./controllers"
